dec98d9298776b337f29d7fa5c71669ee3b240be
Rabire <rabireh@outlook.fr>
2020-12-25 16:51:30 +0100

    another try catch

--- src/scraping/getFollowers.js
+++ src/scraping/getFollowers.js
@@ -1,32 +1,36 @@
 module.exports = async (page, target) => {
-  await page.goto(`https://www.instagram.com/${target}`, {
-    waitUntil: "networkidle0",
-  });
-
-  const targetFollowersCount = await page.evaluate(
-    () =>
-      document.querySelector('ul[class="k9GMp "]').children[1].children[0]
-        .children[0].innerText
-  );
-
-  await page.click('a[class="-nal3 "]'); // followers link
-
-  await page.waitForSelector('div[class="isgrP"]'); // scrollable followers modal
-
-  let followersUsernames = [];
-  while (followersUsernames.length < targetFollowersCount) {
-    await page.evaluate(() => {
-      document.querySelector('div[class="isgrP"]').scroll(0, 120000); // scrollable followers modal
+  try {
+    await page.goto(`https://www.instagram.com/${target}`, {
+      waitUntil: "networkidle0",
     });
 
-    followersUsernames = await page.evaluate(() => {
-      let followersUsernamesFromWeb = Array.from(
-        document.querySelectorAll('a[class="FPmhX notranslate  _0imsa "]') // usernames list
-      );
-      const followersUsernames = [...followersUsernamesFromWeb];
-      return followersUsernames.map((follower) => follower.innerText);
-    });
+    const targetFollowersCount = await page.evaluate(
+      () =>
+        document.querySelector('ul[class="k9GMp "]').children[1].children[0]
+          .children[0].innerText
+    );
+
+    await page.click('a[class="-nal3 "]'); // followers link
+
+    await page.waitForSelector('div[class="isgrP"]'); // scrollable followers modal
+
+    let followersUsernames = [];
+    while (followersUsernames.length < targetFollowersCount) {
+      await page.evaluate(() => {
+        document.querySelector('div[class="isgrP"]').scroll(0, 120000); // scrollable followers modal
+      });
+
+      followersUsernames = await page.evaluate(() => {
+        let followersUsernamesFromWeb = Array.from(
+          document.querySelectorAll('a[class="FPmhX notranslate  _0imsa "]') // usernames list
+        );
+        const followersUsernames = [...followersUsernamesFromWeb];
+        return followersUsernames.map((follower) => follower.innerText);
+      });
+    }
+
+    return followersUsernames;
+  } catch {
+    throw "getFollowers error";
   }
-
-  return followersUsernames;
 };
