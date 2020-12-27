3171820ebbdf38926026a63d5142ff7930e7d86c
Rabire <rabireh@outlook.fr>
2020-12-27 19:51:37 +0100

    colors & text

--- InstalkApp/app/theme/palette.ts
+++ InstalkApp/app/theme/palette.ts
@@ -1,9 +1,15 @@
 export const palette = {
+  ugly: "#00FFFF",
   black: "#1d1d1d",
   white: "#ffffff",
   offWhite: "#e6e6e6",
-  orange: "#FBA928",
-  orangeDarker: "#EB9918",
+
+  blackRussian: "#202124",
+  vulcan: "#37383D",
+
+  summerSky: "#3FA9F5",
+  heliotrope: "#EA80FC",
+
   lightGrey: "#939AA4",
   lighterGrey: "#CDD4DA",
   angry: "#dd3333",
