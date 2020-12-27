9636ed096b6aa0158bb21339842747dc8286c4e1
Rabire <rabireh@outlook.fr>
2020-12-27 19:36:06 +0100

    login screen created

--- InstalkApp/app/navigation/primary-navigator.tsx
+++ InstalkApp/app/navigation/primary-navigator.tsx
@@ -1,28 +1,28 @@
 import React from "react"
-
 import { createNativeStackNavigator } from "react-native-screens/native-stack"
-import { WelcomeScreen } from "../screens"
+import { WelcomeScreen, LoginScreen } from "../screens"
 
 export type PrimaryParamList = {
   welcome: undefined
+  login: undefined
 }
 
-// Documentation: https://github.com/software-mansion/react-native-screens/tree/master/native-stack
 const Stack = createNativeStackNavigator<PrimaryParamList>()
 
 export function PrimaryNavigator() {
   return (
     <Stack.Navigator
-      initialRouteName="welcome"
+      initialRouteName="login"
       screenOptions={{
         headerShown: false,
         gestureEnabled: true,
       }}
     >
+      <Stack.Screen name="login" component={LoginScreen} />
       <Stack.Screen name="welcome" component={WelcomeScreen} />
     </Stack.Navigator>
   )
 }
 
-const exitRoutes = ["welcome"]
+const exitRoutes = ["login"]
 export const canExit = (routeName: string) => exitRoutes.includes(routeName)
---
+++ InstalkApp/app/screens/login-screen/login-screen.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { Screen, Text } from "../../components"
+
+export const LoginScreen = () => {
+  return (
+    <Screen preset="scroll">
+      <Text text="loginScreen" />
+    </Screen>
+  )
+}
