f127189b9bbb46a3183194d127ed638c63231c0d
Rabire <rabireh@outlook.fr>
2020-12-27 20:46:54 +0100

    logo imported

--- InstalkApp/app/screens/login-screen/login-screen.tsx
+++ InstalkApp/app/screens/login-screen/login-screen.tsx
@@ -1,10 +1,13 @@
 import React from "react"
 import { Screen, Text } from "../../components"
+import { InstalkLogoBig } from "../../components/svg/InstalkLogoBig"
+
 import { color } from "../../theme"
 
 export const LoginScreen = () => {
   return (
-    <Screen preset="scroll">
+    <Screen preset="fixed" style={{ alignItems: "center", justifyContent: "space-around" }}>
+      <InstalkLogoBig />
       <Text text="loginScreen" />
       <Text tx="common.next" style={{ color: color.secondary }} />
       <Text tx="common.next" style={{ color: color.tertiary }} />
