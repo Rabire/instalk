7ecac7f5596cf8a2cb439f2e8b4a3be90efa20a2
Rabire <rabireh@outlook.fr>
2020-12-30 00:57:36 +0100

    create route  ok

--- backend/src/controllers/users-controller.js
+++ backend/src/controllers/users-controller.js
@@ -54,3 +54,56 @@ exports.login = async (req, res) => {
       return res.status(401).send("unknown username or password");
     });
 };
+
+exports.create = async (req, res) => {
+  // -- Body verification --
+  const bodyReceived = req.body;
+
+  if (
+    !bodyReceived.username &&
+    !bodyReceived.email &&
+    !bodyReceived.password &&
+    !bodyReceived.penisSize
+  ) {
+    return res.status(401).send("one or multiple required fields are empty");
+  }
+
+  // TODO: check email is unique
+
+  const userToCreate = {
+    username: bodyReceived.username,
+    email: bodyReceived.email,
+    password: hash.createHash(bodyReceived.password),
+    gender: bodyReceived.gender,
+    penisSize: bodyReceived.penisSize,
+  };
+
+  try {
+    const userCreated = await User.create(userToCreate);
+
+    // TODO: token stuff
+    const date = new Date();
+    const expirationDate = date.setDate(date.getDate() + 1);
+    const userToken = jwt.sign({ userId: userCreated.id }, "crypt");
+
+    const updateToken = {
+      token: userToken,
+      tokenExpiration: expirationDate,
+    };
+
+    await User.update(updateToken, {
+      where: { id: userCreated.id },
+    });
+
+    const finalUser = await User.findOne({
+      where: {
+        id: userCreated.id,
+      },
+    });
+
+    return res.status(201).send({ user: finalUser });
+  } catch (err) {
+    console.log(err);
+    return res.sendStatus(500);
+  }
+};
--- backend/src/routes/users-route.js
+++ backend/src/routes/users-route.js
@@ -2,4 +2,6 @@ module.exports = (app) => {
   const users = require("../controllers/users-controller.js");
 
   app.post("/user/login", users.login);
+
+  app.post("/user/", users.create);
 };
