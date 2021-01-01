e501de8df7062784d1c8fca057febaed8ba88434
Rabire <rabireh@outlook.fr>
2021-01-01 16:27:05 +0100

    track screen skelton ok

---
+++ InstalkApp/app/screens/tracks-screen/tracks-screen.styles.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components/native"
+import { vw } from "../../utils/viewport-units"
+
+export const NavBarPlaceholder = styled.View`
+  height: 65px;
+  margin-top: ${vw(17)}px;
+  background-color: red;
+`
+
+export const ScrollView = styled.ScrollView`
+  flex: 1;
+  width: 100%;
+  padding: ${vw(17)}px;
+  margin-bottom: 65px;
+`
+
+export const InnerScrollViewBox = styled.View`
+  align-items: center;
+  flex: 1;
+  background-color: red;
+`
--- InstalkApp/app/screens/tracks-screen/tracks-screen.tsx
+++ InstalkApp/app/screens/tracks-screen/tracks-screen.tsx
@@ -1,10 +1,65 @@
 import React from "react"
-import { Screen, Text } from "../../components"
+import {} from "react-native"
+import { Screen, Text, NavBar } from "../../components"
+import { InstalkLogoMeduim } from "../../components/svg"
+import { NavBarPlaceholder, ScrollView, InnerScrollViewBox } from "./tracks-screen.styles"
+import { vw } from "../../utils/viewport-units"
 
 export const TracksScreen = () => {
   return (
-    <Screen preset="scroll">
-      <Text text="tracksScreen" />
+    <Screen preset="fixed">
+      <ScrollView>
+        <InnerScrollViewBox>
+          <InstalkLogoMeduim customStyles={{ marginBottom: vw(17) }} />
+
+          <Text text="zbi" />
+          <Text text="zbi" />
+          <Text text="zbi" />
+          <Text text="zbi" />
+          <Text text="zbi" />
+          <Text text="zbi" />
+          <Text text="zbi" />
+          <Text text="zbi" />
+          <Text text="zbi" />
+          <Text text="zbi" />
+          <Text text="zbi" />
+          <Text text="zbi" />
+          <Text text="zbi" />
+          <Text text="zbi" />
+          <Text text="zbi" />
+          <Text text="zbi" />
+          <Text text="zbi" />
+          <Text text="zbi" />
+          <Text text="zbi" />
+          <Text text="zbi" />
+          <Text text="zbi" />
+          <Text text="zbi" />
+          <Text text="zbi" />
+          <Text text="zbi" />
+          <Text text="zbi" />
+          <Text text="zbi" />
+
+          <Text text="zbi" />
+          <Text text="zbi" />
+          <Text text="zbi" />
+          <Text text="zbi" />
+          <Text text="zbi" />
+          <Text text="zbi" />
+          <Text text="zbi" />
+          <Text text="zbi" />
+          <Text text="zbi" />
+          <Text text="zbi" />
+
+          <Text text="zbi" />
+          <Text text="zbi" />
+          <Text text="zbi" />
+          <Text text="zb0" />
+
+          <NavBarPlaceholder />
+        </InnerScrollViewBox>
+      </ScrollView>
+
+      <NavBar activeScreen="home" />
     </Screen>
   )
 }
