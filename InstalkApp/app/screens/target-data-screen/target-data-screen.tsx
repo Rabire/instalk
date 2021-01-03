3b83b314e3891ef67aebd0d6ff0b869840ca6f82
Rabire <rabireh@outlook.fr>
2021-01-03 17:49:28 +0100

    error handler

--- InstalkApp/app/screens/target-data-screen/target-data-screen.tsx
+++ InstalkApp/app/screens/target-data-screen/target-data-screen.tsx
@@ -12,6 +12,8 @@ import {
   GreenText,
   ContentBox,
 } from "./target-data-screen.styles"
+import { color } from "../../theme"
+import { SmallText } from "../../enum/styles"
 import { getNewAndMissing } from "../../utils/stats"
 import moment from "moment"
 
@@ -25,12 +27,18 @@ export const TargetDataScreen = (props: TargetDataScreenProps) => {
   const [isLoading, setIsLoading] = useState(false)
   const [targetProfile, setTargetProfile] = useState(null)
   const [targetDatas, setTargetDatas] = useState([])
+  const [i18nError, setI18nError] = useState(null)
+  const [comparisonArray, setComparisonArray] = useState([])
 
   const targetId = route?.params ? route.params.targetId : null
 
   const instalkApi = new Api()
 
+  // console.log({ targetProfile })
+
   useEffect(() => {
+    console.log("useEffect")
+
     setIsLoading(true)
 
     instalkApi
@@ -40,47 +48,50 @@ export const TargetDataScreen = (props: TargetDataScreenProps) => {
           setTargetProfile(response.data.target)
           setTargetDatas(response.data.targetDatas)
         } else {
-          // setI18nError("errors.basicError")
+          setI18nError("errors.basicError")
         }
       })
       .catch(() => {
-        // setI18nError("errors.basicError")
+        setI18nError("errors.basicError")
       })
       .finally(() => setIsLoading(false))
   }, [])
 
-  const comparisonArray = []
+  useEffect(() => {
+    if (targetDatas.length >= 2) {
+      console.log("IFFFFFFFFFF")
+      const _comparisonArray = []
+      for (let i = 1; i < targetDatas.length; i++) {
+        const previousDataIndex = targetDatas.length - i - 1
+        const recentDataIndex = targetDatas.length - i
 
-  if (targetDatas.length >= 2) {
-    // can do some stats
-    for (let i = 1; i < targetDatas.length; i++) {
-      const previousDataIndex = targetDatas.length - i - 1
-      const recentDataIndex = targetDatas.length - i
+        const followers = getNewAndMissing(
+          targetDatas[previousDataIndex].followers,
+          targetDatas[recentDataIndex].followers,
+        )
 
-      const followers = getNewAndMissing(
-        targetDatas[previousDataIndex].followers,
-        targetDatas[recentDataIndex].followers,
-      )
+        const following = getNewAndMissing(
+          targetDatas[targetDatas.length - i - 1].following,
+          targetDatas[targetDatas.length - i].following,
+        )
 
-      const following = getNewAndMissing(
-        targetDatas[targetDatas.length - i - 1].following,
-        targetDatas[targetDatas.length - i].following,
-      )
+        const comparison = {
+          start: targetDatas[previousDataIndex].createdAt,
+          end: targetDatas[recentDataIndex].createdAt,
+          newFollowers: followers.new,
+          missingFollowers: followers.missing,
+          newFollowing: following.new,
+          missingFollowing: following.missing,
+        }
 
-      const comparison = {
-        start: targetDatas[previousDataIndex].createdAt,
-        end: targetDatas[recentDataIndex].createdAt,
-        newFollowers: followers.new,
-        missingFollowers: followers.missing,
-        newFollowing: following.new,
-        missingFollowing: following.missing,
+        _comparisonArray.push(comparison)
+        setComparisonArray([..._comparisonArray])
+
+        console.log({ _comparisonArray })
+        console.log({ comparisonArray })
       }
-
-      comparisonArray.push(comparison)
     }
-  } else {
-    // come back later, not inof exports
-  }
+  }, [targetDatas])
 
   return (
     <Screen preset="fixed">
@@ -89,6 +100,11 @@ export const TargetDataScreen = (props: TargetDataScreenProps) => {
           <InstalkLogoMeduim customStyles={{ marginBottom: 30 }} />
 
           <LoadingWheel isVisible={isLoading} />
+          {i18nError && <SmallText tx={i18nError} style={{ color: color.error }} />}
+
+          {targetDatas.length < 2 && !isLoading && !i18nError && (
+            <SmallText tx="tracksScreen.notEnoughData" style={{ color: color.dim }} />
+          )}
 
           {comparisonArray.map((comparison) => (
             <ContentBox key={comparison.start}>
